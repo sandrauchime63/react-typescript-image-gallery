da44d1a0d6f0cf26ef97cec64451f61f7e56f28a
Decagon <decagon@Decagons-MacBook-Air.local>
2024-01-04 22:07:59 +0100

    implemented the image gallery

---
+++ react-typescript-image-gallery-complete-main/src/data.ts
@@ -0,0 +1,64 @@
+import { IImageGallery } from "./types/global.types";
+
+export const initialImageData: IImageGallery[] = [
+  {
+    id: "1",
+    slug: "/assets/images/pic.jpg",
+    isSelected: false,
+  },
+  {
+    id: "2",
+    slug: "/assets/images/pic1.jpg",
+    isSelected: false,
+  },
+  {
+    id: "3",
+    slug: "/assets/images/pic2.jpg",
+    isSelected: false,
+  },
+  {
+    id: "4",
+    slug: "/assets/images/pic3.jpg",
+    isSelected: false,
+  },
+  {
+    id: "5",
+    slug: "/assets/images/pic4.jpg",
+    isSelected: false,
+  },
+  {
+    id: "6",
+    slug: "/assets/images/pic5.jpg",
+    isSelected: false,
+  },
+  {
+    id: "7",
+    slug: "/assets/images/pic6.jpg",
+    isSelected: false,
+  },
+  {
+    id: "8",
+    slug: "/assets/images/pic7.jpg",
+    isSelected: false,
+  },
+  {
+    id: "9",
+    slug: "/assets/images/pic8.jpg",
+    isSelected: false,
+  },
+  {
+    id: "10",
+    slug: "/assets/images/pic9.jpg",
+    isSelected: false,
+  },
+  {
+    id: "11",
+    slug: "/assets/images/pic10.jpg",
+    isSelected: false,
+  },
+  {
+    id: "12",
+    slug: "/assets/images/pic11.jpg",
+    isSelected: false,
+  }
+];
